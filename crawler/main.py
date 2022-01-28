801606cde12bb6847c4dbfa792b197da1bb750dd
thr-0-jeon <thr_0.jeon@worksmobile.com>
2022-01-28 23:02:28 +0900

    main 함수 정리

--- crawler/main.py
+++ crawler/main.py
@@ -3,7 +3,8 @@ from crawler.sites.indeed import Indeed
 from crawler.sites.dice import Dice
 from crawler.sites.flexjobs import FlexJobs
 
-FIXED_KEYWORD = ["python", "php", "java", "golang", "nest", "spring", "react", "vue"]
+# FIXED_KEYWORD = ["python", "php", "java", "golang", "nest", "spring", "react", "vue"]
+FIXED_KEYWORD = ["python"]
 
 if __name__ == "__main__":
 
