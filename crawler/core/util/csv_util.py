9b948c515c72430545652e8df6cadec26f1242e7
thr-0-jeon <thr_0.jeon@worksmobile.com>
2022-01-29 01:24:29 +0900

    flask 환경 구축 및 파일 저장 API 추가

--- crawler/core/util/csv_util.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-
-def make_csv(file_name, col, data_list: list):
-    with open(f'{file_name}.csv','w', encoding='utf-8') as csv_file :
-        write = csv.writer(csv_file)
-        write.writerow(col)
-        for data in data_list:
-            write.writerow(data)
