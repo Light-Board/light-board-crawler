a11a0b0fb68ccaaca1388cbf5d785fa1df0836b9
Young__0_o <seyoung0254@hanmail.net>
2022-01-29 01:57:37 +0900

    Flask 환경 구축 및 파일 저장 API 추가 (#6)
    * main 함수 정리
    * flexjobs last page 조회 오류 수정
    * flask 환경 구축 및 파일 저장 API 추가
    * 잘 못 생성된 json 파일 제거
    * flask, crawler 가상환경 각자 구축

---
+++ backEnd/src/app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, send_file
+from backEnd.src.util.file_util import save_csv, save_json
+import datetime
+
+app = Flask("SuperScrapper")
+
+
+@app.route("/api/search", methods=['GET'])
+def searchJobs():
+    keyword = request.args.get('keyword')
+    page = request.args.get('page')
+    row = 20
+
+    if not page:
+        page = 1
+
+    if keyword:
+        keyword = keyword.lower()
+
+    # 1. 해당 keyword가 DB에 존재하는가?
+
+    # 1-1. 존재한다면 DB 데이터 조회해서 반환하기
+
+    # 1-2. 존재하지 않는다면, 크롤링 진행 후 긁어온 데이터 DB 저장하고 반환하기
+
+
+@app.route("/api/export", methods=['GET'])
+def exportJobs():
+    keyword = request.args.get('keyword')
+    type = request.args.get('type')
+
+    if not keyword:
+        raise Exception()
+
+    keyword = keyword.lower()
+
+    # keyword에 대해, DB 조회
+    jobs = [{"title": "python developer", "company": "naver", "location": "sungNam", "link": "https://www.naver.com"}]
+
+    if not jobs:
+        raise Exception()
+
+    file_name = f"{keyword}_{datetime.datetime.now()}"
+
+    if type == "csv":
+        save_csv(file_name, ["Title", "Company", "Location", "Link"], jobs)
+        return send_file(f"{file_name}.csv")
+    elif type == "json":
+        save_json(file_name, jobs)
+        return send_file(f"{file_name}.json")
+
+
+app.run(port=8080)
