d652745f7d52370b7c2798a404824f26c717f3c8
thr-0-jeon <thr_0.jeon@worksmobile.com>
2022-01-28 23:02:28 +0900

    main 함수 정리

--- crawler/main_once.py
+++ crawler/main_once.py
@@ -11,48 +11,19 @@ if __name__ == "__main__":
     dice_crawler = Dice()
     flexjobs_crawler = FlexJobs()
 
-    '''
-    아래코드는 테스트용
-    '''
-    # stack_overflow_last_page = stack_overflow_crawler.get_last_page('python')
-    # stack_overflow_jobs = stack_overflow_crawler.extract_jobs("python", 1)
+    all_jobs = []
 
     for keyword in FIXED_KEYWORD:
-        indeed_last_page = indeed_crawler.get_last_page(keyword)
-        print('낑', indeed_last_page)
+        stack_overflow_jobs = stack_overflow_crawler.extract_jobs(keyword, 1)
+        indeed_jobs = indeed_crawler.extract_jobs(keyword, 1)
+        dice_jobs = dice_crawler.extract_jobs(keyword, 1)
+        flexjobs_jobs = flexjobs_crawler.extract_jobs(keyword, 1)
 
-    # indeed_jobs = indeed_crawler.extract_jobs('python', 1)
+        all_jobs += (stack_overflow_jobs + indeed_jobs + dice_jobs + flexjobs_jobs)
 
-    # dice_jobs = dice_crawler.extract_jobs("python", 5)
-    #
-    # flexjobs_jobs = flexjobs_crawler.extract_jobs("python", 3)
+    print(len(all_jobs))
 
-    # print("==================================================================")
-    # print(stack_overflow_jobs)
-    # print("==================================================================")
 
-    # print("==================================================================")
-    # print(dice_jobs)
-    # print("==================================================================")
-    # print(flexjobs_jobs)
-
-    '''
-    # 처음에 전체 페이지 크롤링할 때, 사용할 코드
-    stack_overflow_jobs = []
-    for keyword in FIXED_KEYWORD:
-        stack_overflow_last_page = stack_overflow_crawler.get_last_page(keyword)
-        stack_overflow_jobs.append(stack_overflow_crawler.extract_jobs(keyword, stack_overflow_last_page))
-        indeed_last_page = indeed_crawler.get_last_page(keyword)
-        indeed_jobs = indeed_crawler.extract_jobs(keyword, indeed_last_page)
-    '''
-
-    '''
-    # crontab 통해서 첫 페이지만 크롤링할 경우 사용하는 코드
-    stack_overflow_jobs = []
-    for keyword in FIXED_KEYWORD:
-        stack_overflow_jobs.append(stack_overflow_crawler.extract_jobs(keyword, 1))
-        indeed_jobs.append(indeed_crawler.extract_jobs(keyword, 1))
-    '''
 
 
 
